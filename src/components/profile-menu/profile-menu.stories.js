121b6be0f15949942f59bb42bef4303385771c94
leonardpaulus <leonard.paulus@agenturignis.de>
2021-10-08 13:14:26 +0200

    Add Profile-Menu

---
+++ src/components/profile-menu/profile-menu.stories.js
@@ -0,0 +1,9 @@
+import "./profile-menu.css";
+import profileMenu from "./profile-menu.html";
+
+export default {
+  title: "Components/Profile Menu",
+  parameters: { layout: "centered" },
+};
+
+export const secondary = () => profileMenu;
