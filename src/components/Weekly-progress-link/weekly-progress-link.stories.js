572565b6eab581f825c827aa2ae5560deed54e38
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-10-11 12:23:53 +0200

    Rename button to link

---
+++ src/components/Weekly-progress-link/weekly-progress-link.stories.js
@@ -0,0 +1,9 @@
+import "./weekly-progress-link.css";
+import weeklyProgress from "./weekly-progress-link.html";
+
+export default {
+  title: "Components/Weekly Progress Link",
+  parameters: { layout: "centered" },
+};
+
+export const primary = () => weeklyProgress;
